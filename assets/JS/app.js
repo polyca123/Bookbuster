51c5a23ba8209de83a56a73b10a4df3c75c61966
Polyca <polyca123@gmail.com>
2021-08-25 14:49:19 -0700

    Javascript adjusted

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -26,6 +26,9 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 // Movie api key = 7c6c005c
+const axios = window.axios
+const localStorage = window.localStorage
+
 let movie = []
 const saved = JSON.parse(localStorage.getItem('saved')) || []
 
@@ -34,39 +37,110 @@ document.getElementById('mainSearch').addEventListener('click', event => {
 
   const searchInput = document.getElementById('searchInput').value
 
-  axios.get(`http://www.omdbapi.com/?apikey=7c6c005c&t=${searchInput}`)
+  document.getElementById('mainSection').innerHTML = ''
+
+  axios.get(`http://www.omdbapi.com/?apikey=7c6c005c&s=${searchInput}`)
     .then(movieRes => {
       let movies = movieRes.data.Search
-      
+      console.log(movieRes)
       document.getElementById('searchInput').value = ''
-      console.log(res)
+
+      let movieOutput = ''
 
       document.getElementById('movies').innerHTML = ''
 
-
       movies.forEach(movie => {
-        document.getElementById('movie').innerHTML = `
-      
-      `
-      })
 
-      document.getElementById('movie').value = ''
+        movieOutput +=
+          `
+              <div class="card cd">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="${movie.Poster}" alt="${movie.Title}">
+                  </figure>
+                </div>
+                <div class="card-content ct">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">${movie.Title}</p><br>
+                      <h4 class="subtitle is-4">Released Year: ${movie.Year}</h4>
+                    </div>
+                  </div>
+                  <div class="content">
+                    <h5>IMDB ID: ${movie.imdbID}</h5>
+                    <br>
+                  </div>
+                  <div class="icos">
+
+                    <button class="button favMovie" type="submit" data-id="fav${movie.imdbID}">
+                      <ion-icon name="bookmark-outline"></ion-icon>
+                    </button>
+
+                    <button class="button saveMovie" type="submit"  data-id="saved${movie.imdbID}">
+                      <ion-icon name="heart-outline"></ion-icon>
+                    </button>
+
+                  </div>
+                </div>
+              </div>
+        `
+      })
+      document.getElementById('movies').innerHTML = movieOutput
+
+      // document.getElementById('movie').value = ''
     })
     .catch(err => console.log(err))
 
   axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`)
-    .then(res => {
+    .then(bookRes => {
       document.getElementById('searchInput').value = ''
-      console.log(res)
+      console.log(bookRes);
+      let books = bookRes.data.items
+      let bookOutput = ''
 
-      document.getElementById('book').innerHTML = ''
+      document.getElementById('books').innerHTML = ''
 
-      let book = res.data.items[0]
+      books.forEach(book => {
 
-      document.getElementById('book').innerHTML =`
-      
-      `
+        bookOutput +=
+          `
+            <div class="card cd">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="card-content ct">
+                <div class="media">
+                  <div class="media-content">
+                    <p class="title is-4">${book.volumeInfo.title}</p><br>
+                    <p class="subtitle is-6"><strong>Authors: </strong>${book.volumeInfo.authors}</p>
+                    <h5 class="subtitle is-5">Published Date: ${book.volumeInfo.publishedDate}</h5>
+                  </div>
+                </div>
+                <div class="content">
+                  <p>${book.volumeInfo.description}<p>
+                  <br>
+                  <p><strong>Publisher: </strong>${book.volumeInfo.publisher}</p>
+                  <br>
+                </div>
 
+                <div class="icos">
+                  <button class="button favBook" type="submit" data-id="fav${book.id}">
+                  <ion-icon name="bookmark-outline"></ion-icon>
+                  </button>
+
+                  <button class="button saveBook" type="submit" data-id="saved${book.id}">
+                  <ion-icon name="heart-outline"></ion-icon>
+                  </button>
+
+                </div>
+              </div>
+            </div>
+          `
+
+      })
+      document.getElementById('books').innerHTML = bookOutput
     })
     .catch(err => console.log(err))
 })
