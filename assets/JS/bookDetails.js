f40c2468541616633c654554429bfeba64f05134
Polyca <polyca123@gmail.com>
2021-08-26 20:15:37 -0700

    Book details styling page adjusted

--- assets/JS/bookDetails.js
+++ assets/JS/bookDetails.js
@@ -1,43 +1,43 @@
 // function to get the movie from the id that was set to local storage
 function getBook() {
- let bookId = localStorage.getItem('bookId')
- console.log(bookId);
+  let bookId = localStorage.getItem('bookId')
+  console.log(bookId);
 
- axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookId}`)
-  .then(res => {
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookId}`)
+    .then(res => {
 
-   console.log(res);
+      console.log(res);
 
-   // output the axios response information for this movie id to a new page and add the html
-   let book = res.data.items[0]
-   let output = `
-    <div class="column is-narrow is-centered is-mobile" >
+      // output the axios response information for this movie id to a new page and add the html
+      let book = res.data.items[0]
+      let output = `
+    <div class="column is-narrow is-centered is-mobile is-three-fifths is-offset-one-quarter" >
       <div class="card fullSize cd">
         <div class="card-image">
           <figure class="image is-4by3">
             <img src="${book.volumeInfo.imageLinks.smallThumbnail}" alt="${book.volumeInfo.title}" class="bookImg">
           </figure>
         </div>
-          <div class="card-content ct">
+          <div class="card-content ct detail">
             <div class="media">
               <div class="media-content">
-                <p>Title: ${book.volumeInfo.title}</p>
-                <p>Maturity Rating: ${book.volumeInfo.maturityRating}</p>
+                <p><strong>Title: </strong>${book.volumeInfo.title}</p>
+                <p><strong>Maturity Rating: </strong>${book.volumeInfo.maturityRating}</p>
                 <hr>
-                <h5>Authors: ${book.volumeInfo.authors}</h5>
+                <h5><strong>Authors: </strong>${book.volumeInfo.authors}</h5>
+                <h5><strong>Publisher: </strong>${book.volumeInfo.publisher}</h5>
                 <hr>
                 <p>${book.volumeInfo.description}</p>
                 <br>
-                <p>Page Count: ${book.volumeInfo.pageCount}</p>
-                <p>Published Date: ${book.volumeInfo.publishedDate}</p>
+                <p><strong>Page Count: </strong>${book.volumeInfo.pageCount}</p>
+                <p><strong>Published Date: </strong>${book.volumeInfo.publishedDate}</p>
                 <hr>
-                <p>Rating: ${book.volumeInfo.averageRating}</p>
-                <p>Ratings Count: ${book.volumeInfo.ratingsCount}</p>
+                <p><strong>Rating: </strong>${book.volumeInfo.averageRating}/5</p>
               </div>
             </div>
 
             <div class="content">
-              <h3>Google Book ID: ${book.id}</h3>
+              <h4>Google Book ID: ${book.id}</h4>
               <br>
             </div>
               <a href="http://books.google.com/books?id=${bookId}&dq=${bookId}&hl=&source=gbs_api" target="_blank" class="button is-link">View Google Books</a>
@@ -48,9 +48,9 @@ function getBook() {
           </div>
         </div>
  `
-   document.getElementById('bookInfoPage').innerHTML = output
-  })
-  .catch(err => console.log(err));
+      document.getElementById('bookInfoPage').innerHTML = output
+    })
+    .catch(err => console.log(err));
 }
 
 // call the function
